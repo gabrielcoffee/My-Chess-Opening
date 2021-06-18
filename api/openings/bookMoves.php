58c351370d81068d3b3d859fb2bf5f758b9deed3
Hugo Souza <contato@hugosouza.com>
2021-06-18 14:10:00 -0300

    Update bookMoves.php

--- api/openings/bookMoves.php
+++ api/openings/bookMoves.php
@@ -1,9 +1,9 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
-
-header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: https://mychessopening.com");
+header("Vary: Origin");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: GET");
 
 $line = $_GET["line"];
 
